223c17dc938ba13e24bc9c0bcd8fa181f3785aaf
dt08fj8 <dt08fj8@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-01-24 14:27:38 +0000

    Created FileHandlingTests.

---
+++ enduro-project/test/unittest/FileHandlingTest.java
@@ -0,0 +1,43 @@
+package unittest;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import enduro.FileHandler;
+
+public class FileHandlingTest {
+
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void testSingleFinishFile() {
+		String[] args = { "fakeStart.txt", "fakeFinish.txt" };
+		try {
+			FileHandler.main(args);
+			BufferedReader in = new BufferedReader(new FileReader(
+					"ResultFile.txt"));
+			in.readLine();
+			String out = in.readLine();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testMultipleFinishFiles() {
+		String[] args = { "fakeStart.txt", "-m", "fakeFinishList.txt" };
+		try {
+			FileHandler.main(args);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
