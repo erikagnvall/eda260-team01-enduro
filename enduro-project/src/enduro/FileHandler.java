6422bca9185b0204366e1f73a489b0303c5063bf
dt08fj8 <dt08fj8@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-01-24 14:27:21 +0000

    Renamed to FileHandler. Implemented handling of multiple files.

---
+++ enduro-project/src/enduro/FileHandler.java
@@ -0,0 +1,34 @@
+package enduro;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FileHandler {
+	/**
+	 * 
+	 * @param args
+	 *            The first argument is the startFile, and the second argument
+	 *            is the finishFile. A new file, ResultFile, is created
+	 *            containing the total time for each runner. Else if the second
+	 *            argument is -m the third argument will be a file containing a
+	 *            list of finish-files.
+	 * @throws Exception
+	 *             In case of an I/O error.
+	 */
+	public static void main(String[] args) throws Exception {
+		Sorter sorter = new Sorter();
+		String startFile = args[0];
+		sorter.readStartFile(startFile);
+		if (args[1].equals("-m")) {
+			BufferedReader in = new BufferedReader(new FileReader(args[2]));
+			while (in.ready()) {
+				sorter.readFinishFile(in.readLine());
+			}
+		} else {
+			String goalFile = args[1];
+			sorter.readFinishFile(goalFile);
+		}
+		sorter.readNameFile("fakeName.txt");
+		sorter.createResultFile("ResultFile.txt");
+	}
+}
