529f6ff2f7e7cca84076dc0f9a5b3e4ab4b7c564
tn05jc1 <tn05jc1@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-01-24 15:37:52 +0000

    Added some refactoring to Sorter

--- enduro-project/src/enduro/Sorter.java
+++ enduro-project/src/enduro/Sorter.java
@@ -14,50 +14,55 @@ import enduro.racedata.Time;
 import enduro.racedata.TimeData;
 
 /**
- * Program for generating a result file
- * in an Enduro race.
+ * Program for generating a result file in an Enduro race.
  */
 public class Sorter {
-	
+
 	private TimeData timeData;
 	private PersonData personData;
-	
+
 	/**
-	 * Creates a new Sorter object and
-	 * initializes the data structures.
+	 * Creates a new Sorter object and initializes the data structures.
 	 */
 	public Sorter() {
 		timeData = new TimeData();
 		personData = new PersonData();
 	}
-	
+
 	/**
 	 * Reads a file with start times and populates the data structure..
-	 * @param fileName The name of the file.
+	 * 
+	 * @param fileName
+	 *            The name of the file.
 	 */
-	public void readStartFile(String fileName) throws Exception{
+	public void readStartFile(String fileName) throws Exception {
 		ArrayList<String[]> startingTimes = readFile(fileName);
 		for (int i = 0; i < startingTimes.size(); i++) {
 			int startNbr = Integer.parseInt(startingTimes.get(i)[0]);
 			timeData.addStartTime(startNbr, new Time(startingTimes.get(i)[1]));
 		}
 	}
-	
+
 	/**
 	 * Reads a file with finish times and populates the data structure.
-	 * @param fileName The name of the file.
+	 * 
+	 * @param fileName
+	 *            The name of the file.
 	 */
-	public void readFinishFile(String fileName) throws Exception{
+	public void readFinishFile(String fileName) throws Exception {
 		ArrayList<String[]> finishTimes = readFile(fileName);
 		for (int i = 0; i < finishTimes.size(); i++) {
 			int startNbr = Integer.parseInt(finishTimes.get(i)[0]);
 			timeData.addFinishTime(startNbr, new Time(finishTimes.get(i)[1]));
 		}
 	}
+
 	/**
-	 * Reads a file with a start number and a time and returns and ArrayList containing
-	 * String arrays containing the start number and the time.
-	 * @param fileName The name of the file.
+	 * Reads a file with a start number and a time and returns and ArrayList
+	 * containing String arrays containing the start number and the time.
+	 * 
+	 * @param fileName
+	 *            The name of the file.
 	 * @return ArrayList<String[]> containing start numbers and time.
 	 * @throws Exception
 	 */
@@ -65,25 +70,28 @@ public class Sorter {
 		ArrayList<String[]> list = new ArrayList<String[]>();
 
 		BufferedReader in = new BufferedReader(new FileReader(fileName));
-		while(in.ready()){
+		while (in.ready()) {
 			list.add(in.readLine().split("; "));
 		}
 		return list;
 	}
-	
-	public void readNameFile(String fileName) throws Exception{
+
+	public void readNameFile(String fileName) throws Exception {
 		ArrayList<String[]> names = readFile(fileName);
 		for (int i = 0; i < names.size(); i++) {
 			int startNbr = Integer.parseInt(names.get(i)[0]);
 			personData.addName(startNbr, names.get(i)[1]);
 		}
 	}
-	
+
 	/**
-	 * Creates a result file based on the information
-	 * from readStartFile and readFinishFile.
-	 * @param fileName The name of the result file to be created.
-	 * @throws IOException In case of an I/O error.
+	 * Creates a result file based on the information from readStartFile and
+	 * readFinishFile.
+	 * 
+	 * @param fileName
+	 *            The name of the result file to be created.
+	 * @throws IOException
+	 *             In case of an I/O error.
 	 */
 	public void createResultFile(String fileName) throws IOException {
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(
@@ -93,52 +101,51 @@ public class Sorter {
 		while (itr.hasNext()) {
 			int i = itr.next();
 			String name = personData.getName(i);
-			String start = "Start?";
-			String finish = "Slut?";
-			String total = "--.--.--";
+			String start;
+			String finish;
+			String total;
 			StringBuilder trail = new StringBuilder();
 			Time startTime = null;
 			Time finishTime = null;
 			try {
 				startTime = timeData.getStartTime(i).poll();
 				start = startTime.toString();
-				if(timeData.getStartTime(i).size() > 0){
+				if (timeData.getStartTime(i).size() > 0) {
 					trail.append("; Flera starttider?");
-					while(timeData.getStartTime(i).size() > 0) {
+					while (timeData.getStartTime(i).size() > 0) {
 						trail.append(' ');
 						trail.append(timeData.getStartTime(i).poll());
 					}
 				}
 			} catch (NullPointerException e) {
+				start = "Start?";
 			}
-
 			try {
 				finishTime = timeData.getFinishTime(i).poll();
 				finish = finishTime.toString();
-				if(timeData.getFinishTime(i).size() > 0){
+				if (timeData.getFinishTime(i).size() > 0) {
 					trail.append("; Flera måltider?");
-					while(timeData.getFinishTime(i).size() > 0){
+					while (timeData.getFinishTime(i).size() > 0) {
 						trail.append(' ');
 						trail.append(timeData.getFinishTime(i).poll());
 					}
 				}
 			} catch (NullPointerException e) {
+				finish = "Slut?";
 			}
-
 			try {
 				Time totalTime = startTime.getTotalTime(finishTime);
 				total = totalTime.toString();
 				Time fastTime = new Time(0, 15, 0);
-				if(fastTime.compareTo(totalTime) > 0) trail.append("; Omöjlig Totaltid?");
-			} catch (NullPointerException e) {}
-			
-
-				
-				out.println(i +"; "+ name +"; " + total +"; " + start + "; " + finish + trail.toString());
-
+				if (fastTime.compareTo(totalTime) > 0)
+					trail.append("; Omöjlig Totaltid?");
+			} catch (NullPointerException e) {
+				total = "--.--.--";
 			}
-			
+
+			out.println(i + "; " + name + "; " + total + "; " + start + "; "
+					+ finish + trail.toString());
+		}
 		out.close();
 	}
-	
 }
