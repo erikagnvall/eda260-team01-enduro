f509846a424a60499a9da91014640de61723b84e
et05aw7 <et05aw7@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-02 14:54:03 +0000

    regression test updated with all classes.

--- enduro-project/test/regressiontest/testAll.java
+++ enduro-project/test/regressiontest/testAll.java
@@ -3,18 +3,16 @@ package regressiontest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import unittest.FileHandlingTest;
-import unittest.RegistrationTest;
-import unittest.MarathonSorterTest;
-import unittest.RacerDataTest;
-import unittest.TimeTest;
-import unittest.WrongInputTest;
+import unittest.*;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({FileHandlingTest.class,
-					RegistrationTest.class,
+@Suite.SuiteClasses({ClassHandlingTest.class,
+					FileHandlingTest.class,
+					IllegalStarNbrtest.class,
+					LapRaceSorterTest.class,
 					MarathonSorterTest.class,
 					RacerDataTest.class,
+					RegistrationTest.class,
 					TimeTest.class,
 					WrongInputTest.class})
 public class testAll {
