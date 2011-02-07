bc30240ef9402efb94fed95758657a61b59b05ee
et05aw7 <et05aw7@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-07 21:17:07 +0000

    made changes to the comparator because use of unnecessary @Override

--- enduro-project/src/enduro/racer/LapRacerSorter.java
+++ enduro-project/src/enduro/racer/LapRacerSorter.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 
 public class LapRacerSorter implements Comparator<Racer>{
 
-	@Override
 	public int compare(Racer o1, Racer o2) {
 		return o1.numLaps - o2.numLaps;
 	}
