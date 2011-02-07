9f1c04e7b2bd545834c93542142f750b65a1559e
dt08fj8 <dt08fj8@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-07 13:06:41 +0000

    Major refactoring.

--- enduro-project/src/enduro/LapRaceSorter.java
+++ enduro-project/src/enduro/LapRaceSorter.java
@@ -7,14 +7,20 @@ import enduro.racedata.Time;
 
 public class LapRaceSorter extends Sorter {
 
-	int numLaps = 0;
-	ArrayList<Time> times;
+	public int numLaps = 0;
+	ArrayList<Time> sortedTimeList = new ArrayList<Time>();
 	boolean impossibleLapTime = false;
 
+	// Used in sortedList
+	protected int compareType() {
+		return Racer.LAP_COMPARE;
+	}
+
+	// Everything from here on is needed
 	@Override
 	protected String titleRow(Iterator<Integer> itr) {
 		StringBuilder out = new StringBuilder();
-		out.append(this.nameInformation + "#Varv; TotalTid; ");
+		out.append("StartNr; Namn; #Varv; TotalTid; ");
 		numLaps = racerData.getNumberOfLaps(itr.next());
 		for (int i = 1; i <= numLaps; i++) {
 			out.append("Varv");
@@ -33,28 +39,47 @@ public class LapRaceSorter extends Sorter {
 
 	@Override
 	protected String totalTime(int i) {
-		for (Time t : times) {
+		Time start = startTimes.peek();
+		// Generate a sorted ArrayList with finish times
+		PriorityQueue<Time> timeQueue = new PriorityQueue<Time>();
+		for (Time t : racerData.getFinishTime(i)) {
+			timeQueue.offer(t);
+		}
+		sortedTimeList = new ArrayList<Time>();
+		while (timeQueue.peek() != null) {
+			sortedTimeList.add(timeQueue.poll());
+		}
+
+		for (Time t : sortedTimeList) {
 			finishTime = t;
 		}
-		int laps = 0;
-		if (times != null) {
-			laps = times.size();
-		}
+
+		return start.getTotalTime(finishTime) + "; ";
+	}
+
+	private String lapTimes(int racerNumber, StringBuilder trail) {
+		Time lastTime;
 		StringBuilder out = new StringBuilder();
-		out.append(laps);
-		out.append("; ");
-
-		out.append(startTime.getTotalTime(finishTime));
-		out.append("; ");
-
-		Time lastTime = startTime;
-		for (Time t : times) {
+		Iterator<Time> timeItr = sortedTimeList.iterator();
+		Time t;
+		try {
+			lastTime = startTimes.peek();
+		} catch (NullPointerException e) {
+			lastTime = sortedTimeList.get(0);
+			out.append("; ");
+			timeItr.next();
+		}
+		while (timeItr.hasNext()) {
+			t = timeItr.next();
+			if (lastTime.getTotalTime(t).compareTo(new Time(0, 15, 0)) < 0) {
+				trail.append("Omöjlig varvtid?");
+			}
 			out.append(lastTime.getTotalTime(t));
 			lastTime = t;
 			out.append("; ");
 		}
-		if (times.size() < numLaps) {
-			for (int j = times.size(); j < numLaps; j++) {
+		if (sortedTimeList.size() < numLaps) {
+			for (int j = sortedTimeList.size(); j < numLaps; j++) {
 				out.append("; ");
 			}
 		}
@@ -63,56 +88,80 @@ public class LapRaceSorter extends Sorter {
 	}
 
 	@Override
-	protected Time getFinishTime(int i) throws NullPointerException {
-		PriorityQueue<Time> timeQueue = new PriorityQueue<Time>();
-		for (Time t : racerData.getFinishTime(i)) {
-			timeQueue.offer(t);
-		}
-		times = new ArrayList<Time>();
-		while (timeQueue.peek() != null) {
-			times.add(timeQueue.poll());
-		}
-		return racerData.getFinishTime(i).poll();
-	}
+	protected String getData(int racerNumber) {
+		StringBuilder sb = new StringBuilder();
+		StringBuilder trail = new StringBuilder();
 
-	@Override
-	protected String finishTime(int i) {
-		StringBuilder stringBuilder = new StringBuilder();
-		Time tempTime;
-		if (startTime == null) {
-			tempTime = times.get(0);
-			System.out.println("times.get(0) = " + times.get(0).toString());
+		// Num laps
+		if (startTimes == null) {
+			sb.append(racerData.getNumberOfLaps(racerNumber) - 1);
 		} else {
-			tempTime = startTime;
+			sb.append(racerData.getNumberOfLaps(racerNumber));
 		}
-		impossibleLapTime = false;
-		for (Time t : times) {
-			stringBuilder.append(t.toString());
-			stringBuilder.append("; ");
+		sb.append("; ");
 
-			if (tempTime.getTotalTime(t).compareTo(new Time("00.15.00")) < 0) {
-				impossibleLapTime = true;
+		// Total time
+		if (startTimes == null || finishTimes == null) {
+			sb.append("--.--.--; ");
+			if (finishTimes != null) {
+				// Generate a sorted ArrayList with finish times
+				PriorityQueue<Time> timeQueue = new PriorityQueue<Time>();
+				for (Time t : finishTimes) {
+					timeQueue.offer(t);
+				}
+				sortedTimeList = new ArrayList<Time>();
+				while (timeQueue.peek() != null) {
+					sortedTimeList.add(timeQueue.poll());
+				}
 			}
-			tempTime = t;
+		} else {
+			sb.append(totalTime(racerNumber));
 		}
-		stringBuilder
-				.delete(stringBuilder.length() - 2, stringBuilder.length());
 
-		if (times.size() < numLaps) {
-			stringBuilder.append(";");
+		// Lap times
+		if (finishTimes == null) {
+			for (int i = 0; i < numLaps; i++) {
+				sb.append("; ");
+			}
+		} else {
+			sb.append(lapTimes(racerNumber, trail));
+			sb.append("; ");
 		}
-		if (impossibleLapTime) {
-			stringBuilder.append("; ");
-			stringBuilder.append("Omöjlig varvtid?");
+
+		// Start time
+		if (startTimes == null) {
+			sb.append("Start?; ");
+		} else if (startTimes.size() > 1) {
+			sb.append(startTimes.poll());
+			sb.append("; ");
+			trail.append("Flera starttider? ");
+			while (startTimes.size() != 0) {
+				trail.append(startTimes.poll().toString());
+				trail.append(' ');
+			}
+			trail.delete(trail.length() - 1, trail.length());
+		} else {
+			sb.append(startTimes.poll());
+			sb.append("; ");
 		}
-		return stringBuilder.toString();
-	}
 
-	protected String noTotalTime() {
-		return "0; --.--.--";
-	}
-
-	protected int compareType() {
-		return Racer.LAP_COMPARE;
+		// Finish time
+		if (finishTimes == null) {
+			for (int i = 0; i < numLaps - 2; i++) {
+				sb.append("; ");
+			}
+			trail.append("Slut?");
+		} else {
+			while (finishTimes.size() != 0) {
+				sb.append(finishTimes.poll().toString());
+				sb.append("; ");
+			}
+			sb.delete(sb.length() - 2, sb.length());
+		}
+		if (trail.length() != 0) {
+			sb.append("; ");
+			sb.append(trail.toString());
+		}
+		return sb.toString();
 	}
 }
