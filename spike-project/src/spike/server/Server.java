95a747b325614e93897aa8b1f97956e80cd41138
ada09rsi <ada09rsi@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-17 20:08:47 +0000

    Imported everything.

---
+++ spike-project/src/spike/server/Server.java
@@ -0,0 +1,33 @@
+package spike.server;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * Server example, blatently stolen from
+ * http://download.oracle.com/javase/tutorial
+ * /networking/sockets/clientServer.html although somewhat modified.
+ * 
+ * @author Rick
+ * 
+ */
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = null;
+		// Creates a new socket for incoming connections. Both sockets work on
+		// port 44444
+		// TODO: Create new threads for each incoming connection (Client)
+		try {
+			serverSocket = new ServerSocket(44444);
+		} catch (IOException e) {
+			System.out.println("Could not listen on port: 44444");
+			System.exit(-1);
+		}
+		// Creates a new socket for outgoing connections
+	
+		while (true)
+		    new EnduroServerThread(serverSocket.accept(), 1).start();
+
+	//        serverSocket.close();
+	}
+}
