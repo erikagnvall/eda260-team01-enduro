2e544d2c98391bbbfb5fcf3ab049565197a4f7ce
ada09gor <ada09gor@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-21 10:30:11 +0000

    Javadoc added in EnduroServer

--- enduro-project/src/enduro/network/server/EnduroServer.java
+++ enduro-project/src/enduro/network/server/EnduroServer.java
@@ -4,15 +4,15 @@ import java.io.*;
 import java.net.*;
 
 /**
- * Server example, blatently stolen from
- * http://download.oracle.com/javase/tutorial
- * /networking/sockets/clientServer.html although somewhat modified.
- * 
- * @author Rick
+ * Server for EnduroProject, simple method that launches several threads in order for multiple clients to connect
  * 
  */
 public class EnduroServer implements Runnable{
 	ServerSocket serverSocket;
+/**
+ * Sets up the server socket
+ * @param port int containing port address
+ */
 	public EnduroServer(int port){
 		try {
 			serverSocket = new ServerSocket(port);
@@ -21,7 +21,11 @@ public class EnduroServer implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * Main method to launch the server
+	 * @param args array containing one element, the port address
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		if(args.length != 1){
 			System.out.println("Arguments must match \"port\"");
@@ -29,7 +33,9 @@ public class EnduroServer implements Runnable{
 		EnduroServer serv = new EnduroServer(Integer.parseInt(args[0]));
 		serv.run();
 	}
-	
+	/**
+	 * Method to shut down the server
+	 */
 	public void quit(){
 		try {
 			serverSocket.close();
@@ -38,7 +44,9 @@ public class EnduroServer implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+	/**
+	 * Method to run the server instantiates multiple threads
+	 */
 	public void run(){
 		System.out.println("Server is now running on port " + serverSocket.getLocalPort());
 		int counter = 0;
