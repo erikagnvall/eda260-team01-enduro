5fda0d56baeb16e53b6b60c39f67a88b4057101f
ada09gor <ada09gor@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-21 07:57:27 +0000

    blah

--- enduro-project/src/enduro/network/server/Server.java
+++ enduro-project/src/enduro/network/server/Server.java
@@ -14,20 +14,16 @@ import java.net.*;
 public class Server {
 	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = null;
-		// Creates a new socket for incoming connections. Both sockets work on
-		// port 44444
-		// TODO: Create new threads for each incoming connection (Client)
+		
 		try {
 			serverSocket = new ServerSocket(44444);
 		} catch (IOException e) {
 			System.out.println("Could not listen on port: 44444");
 			System.exit(-1);
 		}
-		// Creates a new socket for outgoing connections
 	
 		while (true)
 		    new EnduroServerThread(serverSocket.accept(), 1).start();
 
-	//        serverSocket.close();
 	}
 }
