84c6c70ca848c8df1fe5b7a034862877b60b48c3
ada09gor <ada09gor@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-28 12:47:03 +0000

    Makes super awesome dialogs!

---
+++ enduro-project/src/enduro/EnduroDialogs.java
@@ -0,0 +1,39 @@
+package enduro;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class EnduroDialogs {
+	public static void main(String[] args){
+		showConfirmDialog();
+		showFailDialog();
+	}
+	public static void showConfirmDialog(){
+		EnduroDialogs df = new EnduroDialogs();
+		JOptionPane.showMessageDialog(null, "Sortering klar!", "Sortering klar!", 0, df.getDanishFlag());
+	}
+	public static void showFailDialog(){
+		EnduroDialogs df = new EnduroDialogs();
+		JOptionPane.showMessageDialog(null, "Se debugResult.txt", "Sortering misslyckad!", 0, df.getItsATrap());
+	
+	}
+	public ImageIcon getDanishFlag(){
+		return createImageIcon("/Danish-Flag.jpg", "a danish flag");
+	}
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected ImageIcon createImageIcon(String path,
+	                                           String description) {
+	    java.net.URL imgURL = getClass().getResource(path);
+	    if (imgURL != null) {
+	        return new ImageIcon(imgURL, description);
+	    } else {
+	        System.err.println("Couldn't find file: " + path);
+	        return null;
+	    }
+	}
+	public ImageIcon getItsATrap(){
+		return createImageIcon("/Atrapitis.gif", "It's a trap");
+	}
+	
+
+}
