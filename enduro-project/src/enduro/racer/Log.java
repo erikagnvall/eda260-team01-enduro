d343b3503e007df09bb08f6f0d04884a7af421a5
ada09rsi <ada09rsi@d26981aa-be36-e27e-0d63-b9bc55f13419>
2011-02-28 12:19:44 +0000

    implemented methods for checking log

--- enduro-project/src/enduro/racer/Log.java
+++ enduro-project/src/enduro/racer/Log.java
@@ -8,15 +8,21 @@ public class Log {
 
 	private String location;
 	private static Log logger = null;
+	private static boolean isWritten;
 	
 	private Log(String location) {
 		this.location = location;
 	}
 	
 	public static void log(String data) {
+		isWritten = false;
 		if(logger == null)
 			logger = new Log("log.log");
 		logger.writeToLog(data);
+	}
+	
+	public static boolean checkLog(){
+		return isWritten;
 	}
 	
 	public static void setLogLocation(String location) {
@@ -35,6 +41,7 @@ public class Log {
 	
 	public void writeToLog(String data) {
 		try {
+			isWritten = true;
 			FileWriter writer = new FileWriter(location, true);
 			writer.append(data);
 			writer.append("\n");
